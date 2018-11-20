74c86a9669be0d9855fa20ffce1d15659738be0e
Shane Walsh <studioshane@gmail.com>
2018-11-20 18:12:34 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,36 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  
+   var paragraph = $('p');
+  
+  return paragraph
+
+}
+
+function lastImageSelector(){
+  
+  return $('div img:last-child ')
+  
+  return lastImage
+  
+}
+
+function ninjaBabySelector(){
+  
+  return $('#baby-ninja')
+  
+
+}
+
+function divSelector(){
+  
+   return $('.pics')
+  
+}
+
+function firstListItem(){
+  
+ return $('ul first:pic-list')
+  
+}
